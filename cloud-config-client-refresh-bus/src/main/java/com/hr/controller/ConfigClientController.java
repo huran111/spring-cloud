ad293425f3a1859018f8c16d898264a6e0b9b802
BUCHU <710625937@qq.com>
2019-01-21 17:56:13 +0800

    spring cloud-eureka-bus 客户端自动刷新

---
+++ cloud-config-client-refresh-bus/src/main/java/com/hr/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.hr.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 胡冉
+ * @Description: TODO
+ * @date 2019/1/1511:23
+ * @copyright {@link www.hndfsj.com}
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${profile}")
+    private String profile;
+    @GetMapping(value = "/profile")
+    public String getProFile() {
+        return profile;
+    }
+}
