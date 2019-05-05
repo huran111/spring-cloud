7bb7b17555b27585539de4b6302ad25fcb721f21
BUCHU <710625937@qq.com>
2019-05-05 18:38:38 +0800

    spring cloud-eureka-server 服务端自动刷新

---
+++ cloud-eureka-ha/src/main/java/com/hr/CloudEurekaHaApplication.java
@@ -0,0 +1,17 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class CloudEurekaHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaHaApplication.class, args);
+	}
+
+}
+
